3f741e6704a949807deaa5aea3adf55268d56826
lucatk <mail@lucakillmaier.com>
2020-04-20 11:41:00 +0200

    Add monochrome unicons to generator

--- build/generate.js
+++ build/generate.js
@@ -12,12 +12,11 @@ fs.removeSync(iconsComponentPath)
 fs.mkdirSync(iconsComponentPath)
 
 const indexJs = []
-
-uniconsConfig.forEach(icon => {
-  const baseName = `uil-${icon.name}`
+const iconWorker = (sourcePath, iconKey) => icon => {
+  const baseName = `ui${iconKey}-${icon.name}`
   const location = path.join(iconsComponentPath, `${baseName}.js`)
   const name = upperCamelCase(baseName)
-  const svgFile = fs.readFileSync(path.resolve('node_modules/@iconscout/unicons', icon.svg), 'utf-8')
+  const svgFile = fs.readFileSync(path.resolve(sourcePath, icon.svg), 'utf-8')
 
   let data = svgFile.replace(/<svg[^>]+>/gi, '').replace(/<\/svg>/gi, '')
   // Get Path Content from SVG
@@ -61,8 +60,20 @@ export default ${name};
 
   // Add it to index.js
   indexJs.push(`export { default as ${name} } from './icons/${baseName}'`)
-})
+}
+
+uniconsConfig.forEach(iconWorker('node_modules/@iconscout/unicons', 'l'))
+
+const monochromeSourcePath = process.argv.slice(2)[0]
+if (monochromeSourcePath) {
+  console.log('Generating monochrome icons from svg source:', monochromeSourcePath)
+  const monochromeUniconsConfig = fs.readdirSync(path.resolve(monochromeSourcePath)).map(svg => {
+    const name = path.basename(svg, '.svg')
+    return { name, svg }
+  })
+  monochromeUniconsConfig.forEach(iconWorker(monochromeSourcePath, 'm'))
+}
 
 fs.writeFileSync(iconsIndexPath, indexJs.join('\n'), 'utf-8')
 
-console.log(`Generated ${uniconsConfig.length} icon components.`)
+console.log(`Generated ${indexJs.length} icon components.`)
