80e9ae3a4bcd6bd2271b6d8559f46f4c47b4b564
lucatk <mail@lucakillmaier.com>
2020-07-05 17:24:26 +0200

    Improved package implementation

--- build/generate.js
+++ build/generate.js
@@ -1,79 +1,108 @@
 const path = require('path')
 const fs = require('fs-plus')
 const cheerio = require('cheerio')
-const upperCamelCase = require('uppercamelcase')
 
-const iconsComponentPath = path.join(process.cwd(), 'icons')
-const iconsIndexPath = path.join(process.cwd(), 'index.js')
-const uniconsConfig = require('@iconscout/unicons/json/line.json')
+const iconsJsonPath = path.join(process.cwd(), 'icons')
 
-// Clear Directories
-fs.removeSync(iconsComponentPath)
-fs.mkdirSync(iconsComponentPath)
-
-const indexJs = []
-const iconWorker = (sourcePath, iconKey) => icon => {
-  const baseName = `ui${iconKey}-${icon.name}`
-  const location = path.join(iconsComponentPath, `${baseName}.js`)
-  const name = upperCamelCase(baseName)
+const monochromeClass = fill => {
+  switch ((fill || '').toLowerCase()) {
+  case '#6563ff':
+    return 'uim-primary'
+  case '#a2a1ff':
+    return 'uim-tertiary'
+  case '#c1c0ff':
+    return 'uim-quaternary'
+  }
+}
+const iconsReducer = (sourcePath) => (obj, icon, i) => {
   const svgFile = fs.readFileSync(path.resolve(sourcePath, icon.svg), 'utf-8')
 
   let data = svgFile.replace(/<svg[^>]+>/gi, '').replace(/<\/svg>/gi, '')
+
   // Get Path Content from SVG
   const $ = cheerio.load(data, {
     xmlMode: true
   })
-  const svgPath = $('path').attr('d')
+  const svgElements = $('path, circle')
+  const svg = svgElements.map((_, el) => {
+    const type = $(el)[0].name
+    const cls = monochromeClass($(el).attr('fill'))
+    const opacity = $(el).attr('opacity')
 
-  const template = `import React from "react";
-import Svg, { Path } from "react-native-svg";
-import PropTypes from "prop-types";
+    let str = `<${type} `
+    if (cls) {
+      str += `class="${cls}" `
+    }
 
-const ${name} = props => {
-  const { color, size, ...otherProps } = props;
-  return (
-    <Svg
-      width={size}
-      height={size}
-      viewBox="0 0 24 24"
-      fill={color}
-      {...otherProps}
-    >
-      <Path d="${svgPath}" />
-    </Svg>
-  );
-};
+    if (type === 'path') {
+      const d = $(el).attr('d')
+      if (d) {
+        str += `d="${d}" `
+      }
+    } else if (type === 'circle') {
+      const cx = $(el).attr('cx')
+      const cy = $(el).attr('cy')
+      const r = $(el).attr('r')
+      if (cx) {
+        str += `cx="${cx}" `
+      }
+      if (cy) {
+        str += `cy="${cy}" `
+      }
+      if (r) {
+        str += `r="${r}" `
+      }
+    }
 
-${name}.propTypes = {
-  color: PropTypes.string,
-  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-};
+    if (opacity) {
+      str += `opacity="${opacity}" `
+    }
+    str += '/>'
+    
+    return str
+  }).get().join('')
 
-${name}.defaultProps = {
-  color: "currentColor",
-  size: "24"
-};
-
-export default ${name};
-`
-  fs.writeFileSync(location, template, 'utf-8')
-
-  // Add it to index.js
-  indexJs.push(`export { default as ${name} } from './icons/${baseName}'`)
+  return {
+    ...obj,
+    [icon.name]: svg,
+  }
 }
 
-uniconsConfig.forEach(iconWorker('node_modules/@iconscout/unicons', 'l'))
+const styles = [
+  {
+    name: 'line',
+    source: 'node_modules/@iconscout/unicons',
+    config: require('@iconscout/unicons/json/line.json')
+  },
+]
 
 const monochromeSourcePath = process.argv.slice(2)[0]
 if (monochromeSourcePath) {
-  console.log('Generating monochrome icons from svg source:', monochromeSourcePath)
   const monochromeUniconsConfig = fs.readdirSync(path.resolve(monochromeSourcePath)).map(svg => {
     const name = path.basename(svg, '.svg')
     return { name, svg }
   })
-  monochromeUniconsConfig.forEach(iconWorker(monochromeSourcePath, 'm'))
+  styles.push({
+    name: 'monochrome',
+    source: monochromeSourcePath,
+    config: monochromeUniconsConfig
+  })
+} else {
+  styles.push({
+    name: 'monochrome',
+    source: 'n/a',
+    config: []
+  })
 }
 
-fs.writeFileSync(iconsIndexPath, indexJs.join('\n'), 'utf-8')
+// Clear Directories
+fs.removeSync(iconsJsonPath)
+fs.mkdirSync(iconsJsonPath)
 
-console.log(`Generated ${indexJs.length} icon components.`)
+styles.forEach(style => {
+  console.log(`Generating icons for style "${style.name}" from source ${style.source}...`)
+  const icons = style.config.reduce(iconsReducer(style.source), {})
+  const location = path.join(iconsJsonPath, `${style.name}.json`)
+  fs.writeFileSync(location, JSON.stringify(icons), 'utf-8')
+  console.log(`Generated ${Object.keys(icons).length} icons for style "${style.name}".`)
+})
